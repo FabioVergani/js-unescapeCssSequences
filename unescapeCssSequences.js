9fe4ba4760b16bde51fc70a46a1a66fa32962d9f
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-06-26 10:11:00 +0200

    Create unescapeCssSequences.js

---
+++ unescapeCssSequences.js
@@ -0,0 +1,12 @@
+const RegularExpressions={
+ CSS:{
+	Sequence:/\\((([\dA-Fa-f]{2}|[\dA-Fa-f]{1,5})\s)|([\dA-Fa-f]{6}|[\dA-Fa-f]{4}))/g,
+ }
+};
+
+
+function unescapeCssSequences(s){
+ const f=String.fromCharCode;
+ return s.replace(RegularExpressions.CSS.Sequence,function(c){return f(parseInt(c.slice(2),16));});
+}
+console.log(unescapeCssSequences("a\\0020b\\0022c"));
